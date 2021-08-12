67673a762c905dbe2f84586fd07d79f3b7ef9f29
GeekyPython <shabs1505@gmail.com>
2021-08-12 15:12:33 +0530

    changes made

--- tinder-backend/dbCards.js
+++
@@ -1,9 +0,0 @@
-const {Schema,model} = require('mongoose');
-// import mongoose from 'mongoose';
-
-const cardSchema = Schema({
-    name: String,
-    img: String
-});
-
-module.exports = model('cards',cardSchema);
